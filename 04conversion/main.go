d712332093bd1bbc401257aaac083456368896a6
Bittu-the-coder <Bittuprajapati2271@gmail.com>
2025-02-02 00:58:31 +0530

    `Added input handling and rating calculation to main function`

--- 04conversion/main.go
+++ 04conversion/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to our pizza app")
+	fmt.Println("Please rate our service: ")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Thanks for your feedback", input)
+
+	
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your rating: ", numRating+1)
+	}
+
 }
